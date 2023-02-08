890930d248a04387f9409ae3bde6153c3bdc11ff
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-07 19:27:09 -0600

    initial data and first component

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <LearningResource v-for="res in storedResources" :key="res.id" />
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official documentation of Vue',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn how to Google..',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
