534c1121918d513576bf7e0e209a5bc03137b64b
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-07 19:44:51 -0600

    Added storage styling

--- src/components/learning-resources/StoredResources.vue
+++ src/components/learning-resources/StoredResources.vue
@@ -18,3 +18,13 @@ export default {
   components: { LearningResource },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
