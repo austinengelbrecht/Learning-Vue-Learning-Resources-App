a48cf642bfb7bcd6f3e85b168b59c58b6bab42f5
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-07 19:44:16 -0600

    Refactoring into better component structure

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <LearningResource
+      v-for="res in resources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    />
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+  props: ['resources'],
+  components: { LearningResource },
+};
+</script>
