0bfd687a4092ca339bb28c8ded7e7161de1c13f7
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-07 19:29:19 -0600

    building html structure

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -1,5 +1,19 @@
 <template>
-  <div></div>
+  <li>
+    <div>
+      <header>
+        <h3></h3>
+
+        <button>Delete</button>
+      </header>
+    </div>
+
+    <p></p>
+
+    <nav>
+      <a href="">View Resource</a>
+    </nav>
+  </li>
 </template>
 
 <script>
