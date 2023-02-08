7b115adee1afd59d394807740c2cabaaa47c761b
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-07 19:32:20 -0600

    Pushing data through props

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <ul>
-    <LearningResource v-for="res in storedResources" :key="res.id" />
+    <LearningResource
+      v-for="res in storedResources"
+      :key="res.id"
+      title="res.title"
+      description="res.description"
+      link="res.link"
+    />
   </ul>
 </template>
 
