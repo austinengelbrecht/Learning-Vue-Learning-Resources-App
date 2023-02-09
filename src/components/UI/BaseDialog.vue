4aae6ab8ba373d3a24f1ed59f6fc98fea06d79ff
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-08 20:59:41 -0600

    Basic form validation and dialog ui

---
+++ src/components/UI/BaseDialog.vue
@@ -0,0 +1,17 @@
+<template>
+  <dialog open>
+    <header>
+      <slot>
+        <h2>{{ title }}</h2>
+      </slot>
+    </header>
+  </dialog>
+</template>
+
+<script>
+export default {
+  props: ['title'],
+};
+</script>
+
+<style></style>
--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -37,6 +37,14 @@ export default {
       const enteredDesc = this.$refs.descInput.value;
       const enteredLink = this.$refs.linkInput.value;
 
+      if (
+        enteredTitle.trim() === '' ||
+        enteredDesc.trim() === '' ||
+        enteredLink.trim() === ''
+      ) {
+        return;
+      }
+
       this.addResource(enteredTitle, enteredDesc, enteredLink);
     },
   },
