cb0475bbdd6e6cd2156c90c3d6f0174f9a0adbd9
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-08 20:44:09 -0600

    Build out form and prepare for fall-through

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,6 +1,25 @@
 <template>
   <base-card>
-    <form></form>
+    <form>
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input id="title" name="title" type="text" />
+      </div>
+
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea id="description" name="description" rows="3" />
+      </div>
+
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input id="link" name="link" type="url" />
+      </div>
+
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
   </base-card>
 </template>
 
